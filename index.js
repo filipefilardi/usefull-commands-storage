f7a5225876288a4d1512c10c7f38e6521ee73810
Filipe Filardi <filipe.jesus@usp.br>
2018-03-11 00:58:42 -0300

    add route for client/build

--- index.js
+++ index.js
@@ -25,4 +25,13 @@ app.use(passport.session());
 require("./src/components/user/userAPI")(app);
 require("./src/components/command/commandAPI")(app);
 
+if(process.env.NODE_ENV === 'production'){
+	app.use(express.static('client/build'));
+	
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 app.listen(PORT);
